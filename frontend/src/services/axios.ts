1a925b149109e696eeecbdab63e7e98693070597
VivekBhardwaj80 <vp0444199@gmail.com>
2026-02-05 06:10:30 +0530

    Update base URL for Axios instance

--- frontend/src/services/axios.ts
+++ frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:import.meta.env.VITE_BACKEND_URL,
+    baseURL:https://port-backend-fab0.onrender.com,
     withCredentials:true,
 });
 
