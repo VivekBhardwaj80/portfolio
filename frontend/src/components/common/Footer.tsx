a8b93f70b1373c4eafe6e595da824c3bb0e8fbf5
Vivek Bhardwaj <vp0444199@gmail.com>
2026-02-11 10:42:52 +0530

    check url

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,41 +1,44 @@
-import instagram from "../../assets/instaGram2Png.png";
-import X from "../../assets/XPng.png";
-import facebook from "../../assets/facebook.png";
-import github from "../../assets/github2.png";
-import linkedIn from "../../assets/linkendInPng.png";
+import {
+  FaFacebook,
+  FaGithub,
+  FaHeart,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../app/store";
-import { FaHeart } from "react-icons/fa";
 
 type PublicLayoutProps = {
   darkMode: boolean;
 };
 
-type SocialKey = "instagram" | "X" | "facebook" | "github" | "linkedIn";
-
 const Footer = ({ darkMode }: PublicLayoutProps) => {
   const currentYear = new Date().getFullYear();
-  // const dispatch = useDispatch<AppDispatch>();
-  const { profileList } = useSelector(
-    (state: RootState) => state.profile,
-  );
+  const { profileList } = useSelector((state: RootState) => state.profile);
   const profile = profileList?.[0];
-  // const profileImage = profile?.profileImage;
+  let parsedSocialLinks: Partial<
+    Record<"instagram" | "X" | "facebook" | "github" | "linkedIn", string>
+  > = {};
 
-  const parsedSocialLinks: Partial<Record<SocialKey, string>> =
-    profile?.socialLinks
-      ? typeof profile.socialLinks === "string"
-        ? JSON.parse(profile.socialLinks)
-        : profile.socialLinks
-      : {};
+  try {
+    if (profile?.socialLinks) {
+      parsedSocialLinks =
+        typeof profile.socialLinks === "string"
+          ? JSON.parse(profile.socialLinks)
+          : profile.socialLinks;
+    }
+  } catch {
+    parsedSocialLinks = {};
+  }
 
-  const socialLink: { key: SocialKey; icon: string; alt: string }[] = [
-    { key: "instagram", icon: instagram, alt: "instagram" },
-    { key: "X", icon: X, alt: "X" },
-    { key: "facebook", icon: facebook, alt: "facebook" },
-    { key: "github", icon: github, alt: "github" },
-    { key: "linkedIn", icon: linkedIn, alt: "instagram" },
-  ];
+  const socials = [
+    { key: "instagram", icon: <FaInstagram /> },
+    { key: "X", icon: <FaXTwitter /> },
+    { key: "facebook", icon: <FaFacebook /> },
+    { key: "github", icon: <FaGithub /> },
+    { key: "linkedIn", icon: <FaLinkedin /> },
+  ] as const;
   return (
     <div className="border-t">
       <div className="container mx-auto px-4 py-8">
@@ -60,29 +63,23 @@ const Footer = ({ darkMode }: PublicLayoutProps) => {
             </p>
           </div>
           <div className="flex gap-4">
-            {socialLink.map((social, index) => {
-                  const link = parsedSocialLinks[social.key];
-                  if (!link) return null;
+            {socials.map(({ key, icon }) => {
+              const url = parsedSocialLinks[key];
 
-                  return (
-                    <a
-                      key={index}
-                      href={link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      data-aos-delay={`${400 + index * 100}`}
-                      className="transform hover:scale-110 transition-transform duration-300"
-                    >
-                      <img
-                        src={social.icon}
-                        alt={social.alt}
-                        className={`w-8 h-8 sm:w-10 sm:h-10 object-contain ${
-                          darkMode ? "" : "filter brightness-75"
-                        }`}
-                      />
-                    </a>
-                  );
-                })}
+              if (!url || !url.trim()) return null;
+
+              return (
+                <a
+                  key={key}
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-10 h-10 rounded-full flex items-center justify-center hover:scale-110 transition-all hover:bg-linear-to-r from-cyan-500 to-blue-500 hover:text-black dark:hover:text-white bg-[#374151] dark:bg-[#f3f4f6] text-[#ffffff] dark:text-black"
+                >
+                  {icon}
+                </a>
+              );
+            })}
           </div>
           <div className="text-center md:text-right">
             <p className="text-sm flex items-center justify-end gap-1 text-[#6b7280] dark:text-[#9ca3af]">
